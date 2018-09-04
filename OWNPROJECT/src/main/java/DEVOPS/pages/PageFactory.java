cccfaa553d91bdf634ecfa4b75240a4fff6eab54
ramuduamps <ramuduamps@gmail.com>
2018-09-04 07:55:10 +0000

    ownpro

---
+++ OWNPROJECT/src/main/java/DEVOPS/pages/PageFactory.java
@@ -0,0 +1,17 @@
+package DEVOPS.pages;
+
+import org.jbehave.web.selenium.WebDriverProvider;
+
+public class PageFactory {
+
+    private final WebDriverProvider webDriverProvider;
+
+    public PageFactory(WebDriverProvider webDriverProvider) {
+        this.webDriverProvider = webDriverProvider;
+    }
+
+    public Home newHome() {
+        return new Home(webDriverProvider);
+    }
+
+}
